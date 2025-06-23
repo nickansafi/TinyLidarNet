5632b5301f3479a2900100964d37f2e48d0eb074
nickansafi <nickansafi@Nickans-Air.attlocal.net>
2025-06-23 11:30:16 -0500

    fix

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
-source /opt/ros/noetic/setup.bash
+source ~/.bashrc
 sudo apt-get install -y python3-pip
 cd ROS_Workspace
 catkin_make
 source devel/setup.bash
 cd ..
 pip install rospy rosbag scikit-learn matplotlib
-cd Benchmark/f1tenth_benchmarks
+cd Benchmark
 pip install -r requirements.txt
 pip install -e .
 git submodule init
