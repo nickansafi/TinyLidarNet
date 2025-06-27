6881c9af2bc254da98105d8b1e2629a1a04318eb
nickansafi <nickansafi@outlook.com>
2025-06-27 10:51:01 -0500

    Update ros.sh

--- ros.sh
+++ ros.sh
@@ -2,5 +2,6 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 sudo apt install -y ros-noetic-desktop-full
+sudo apt install -y ros-noetic-roslaunch
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 sudo apt install -y ros-noetic-driver-base
